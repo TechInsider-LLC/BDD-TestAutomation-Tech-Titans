6672cde05443a06adc4012c95efc099a4038513d
Abdumuminova <111468775+Abdumuminova@users.noreply.github.com>
2022-09-02 18:10:19 -0500

    Created a new class

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,4 +1,6 @@
+package stepDefinitions;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -6,10 +8,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 
 public class Hooks {
-   @Before
-   public void setUp(){
+    public static WebDriver driver;
+
+    @Before
+    public void setUp(){
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
+
+    @After
+    public void close(){driver.quit();}
+
+
 }
+
