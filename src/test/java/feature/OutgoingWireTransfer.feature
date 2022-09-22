58d76bd3010c3496b07a66725bbcb5e6f4074317
AzamatSiro <111468715+AzamatSiro@users.noreply.github.com>
2022-09-22 18:32:32 -0400

    wire transfer

--- src/test/java/feature/OutgoingWireTransfer.feature
+++ src/test/java/feature/OutgoingWireTransfer.feature
@@ -3,11 +3,12 @@
 Feature: Outgoing wire Transfer
 
   Scenario: Outgoing Wire Transferring
-    Given User on the Ebanq page
+   Given User on the Ebanq page
     When User Logs in and click on transfers then click on Outgoing wire Transfer
     And Select account and fills the form then click on continue
     And User on Transfer details page Scrolls down end click on continue
     Then Users Transaction success
+#    When Admin approves transaction
 
   Scenario: OutGoing Wire Transfer Unsuccessfully
     Given User on the Ebanq page
--- src/test/java/stepDefinitions/WireTransfer.java
+++ src/test/java/stepDefinitions/WireTransfer.java
@@ -1,19 +1,27 @@
 package stepDefinitions;
-import io.cucumber.java.en.*;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.http.Cookies;
+import io.restassured.response.Response;
+import org.junit.Test;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pageObjects.WireTransferPage;
 
 import java.time.Duration;
 
+import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static utility.Hooks.getDriver;
 
 public class WireTransfer {
+
+    String id;
 
     String username;
     String password;
@@ -43,12 +51,13 @@ public class WireTransfer {
         amountToTransfer = transferPage.amountToTransfer;
         transferPage.load();
     }
+
     @When("User Logs in and click on transfers then click on Outgoing wire Transfer")
     public void user_logs_in_and_click_on_transfers_then_click_on_outgoing_wire_transfer() throws InterruptedException {
 //        Thread.sleep(3000);
         transferPage.usernameInput.sendKeys(transferPage.validUsername);
-        transferPage.passwordInput.sendKeys(transferPage.validPassword,Keys.ENTER);
-       Thread.sleep(4000);
+        transferPage.passwordInput.sendKeys(transferPage.validPassword, Keys.ENTER);
+        Thread.sleep(4000);
 //        transferPage.headerMenu.click();
 //        Thread.sleep(2000);
         transferPage.transfers.click();
@@ -57,6 +66,7 @@ public class WireTransfer {
 
 
     }
+
     @When("Select account and fills the form then click on continue")
     public void select_account_and_fills_the_form_then_click_on_continue() throws InterruptedException {
         Thread.sleep(2000);
@@ -76,25 +86,51 @@ public class WireTransfer {
         transferPage.selectCurrency.click();
         transferPage.selectUSA$.click();
         Thread.sleep(3000);
-        transferPage.body.sendKeys(Keys.CONTROL,Keys.END);
+        transferPage.body.sendKeys(Keys.CONTROL, Keys.END);
         transferPage.continueButton.click();
     }
 
     @When("User on Transfer details page Scrolls down end click on continue")
     public void user_on_transfer_details_page_scrolls_down_end_click_on_continue() throws InterruptedException {
-        transferPage.body.sendKeys(Keys.CONTROL,Keys.END);
+        transferPage.body.sendKeys(Keys.CONTROL, Keys.END);
         Thread.sleep(3000);
         transferPage.continueButton.click();
     }
+
     @Then("Users Transaction success")
     public void users_transaction_success() throws InterruptedException {
 //        Thread.sleep(2000);
         String expected = transferPage.successMessage.trim();
         WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.textToBePresentInElement(transferPage.successMessage1,expected));
+        wait.until(ExpectedConditions.textToBePresentInElement(transferPage.successMessage1, expected));
         String actual = transferPage.successMessage1.getText().trim();
         assertTrue(actual.contains(expected));
+
+        id = actual.split("ID #")[1].split("Back")[0].trim();
     }
+
+    @When("Admin approves transaction")
+    @Test
+    public void admin_approves_transaction() {
+        Response response =
+                given().contentType("application/json")
+                        .body("{\"data\":{\"email\":\"Bank-Admin\",\"password\":\"Demo-Access1\"}}")
+                        .when().post("https://api-demo.ebanq.com/users/public/v1/auth/signin")
+                        .then().log().all().statusCode(200).extract().response();
+
+        String token = response.path("data.accessToken");
+        Cookies cookies = response.getDetailedCookies();
+
+        given().log().all()
+                .contentType("application/json")
+                .header("Authorization", "Bearer " + token)
+                .cookies(cookies)
+                .when().post("https://api-demo.ebanq.com/accounts/private/v1/admin/requests/execute/{id}", id)
+                .then().log().all().statusCode(200);
+
+    }
+
+
 
 
 
